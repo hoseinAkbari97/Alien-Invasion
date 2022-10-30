51618077e1f3d9c1975865c0bb9b7ddaf078d8be
hossein1618 <hosseintestnet1akbari@gmail.com>
2022-10-30 20:41:03 +0330

    Creating the Setting Module

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from setting import Setting
+
 
 class AlienInvasion:
 
@@ -9,11 +11,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 700))
+        self.settings = Setting()
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,
+            self.settings.screen_height
+        ))
         pygame.display.set_caption("Alien Invasion")
-
-        """Setting the background color"""
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -24,7 +27,7 @@ class AlienInvasion:
                     sys.exit()
 
             """Redraw the screen during each pass of the loop"""
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             """Make the most recently drawn screen visible."""
             pygame.display.flip()
