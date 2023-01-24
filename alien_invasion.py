bccc328c664f981bba0f040e49d89a483c37c9f8
hossein1618 <hosseintestnet1akbari@gmail.com>
2023-01-24 23:31:23 +0330

    Firing Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from setting import Setting
 
 from ship import Ship
 
+from bullet import Bullet
+
 
 class AlienInvasion:
 
@@ -49,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key release."""
@@ -57,11 +61,18 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Creating a new bullet and adding it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         """Redraw the screen during each pass of the loop"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         """Make the most recently drawn screen visible."""
         pygame.display.flip()
