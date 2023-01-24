d8755914487bfc25199e9e9347874d67d95f876e
hossein1618 <hosseintestnet1akbari@gmail.com>
2023-01-24 22:56:42 +0330

    Grouping Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,12 +21,14 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
